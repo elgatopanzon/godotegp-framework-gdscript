a0c04d095496c3ed87018031af57d492e1a384f0
ElGatoPanzon <gato@elgatopanzon.io>
2023-09-12 17:59:57 -0600

    refactor: ServiceManagerService class to just Service

---
+++ scripts/services/Service.gd
@@ -0,0 +1,47 @@
+######################################################################
+# @author      : ElGatoPanzon
+# @class       : Service
+# @created     : Friday Sep 08, 2023 17:05:13 CST
+# @copyright   : Copyright (c) ElGatoPanzon 2023
+#
+# @description : Base class for implementing a Service for the ServiceManager
+######################################################################
+
+class_name Service
+extends Node
+
+# object constructor
+func _init():
+	pass
+
+
+# scene lifecycle methods
+# called when node enters the tree
+# func _enter_tree():
+# 	pass
+
+# called once when node is ready
+# func _ready():
+# 	pass
+
+# called when node exits the tree
+# func _exit_tree():
+# 	pass
+
+
+# process methods
+# called during main loop processing
+# func _process(delta: float):
+# 	pass
+
+# called during physics processing
+# func _physics_process(delta: float):
+# 	pass
+
+# called when service has been registered
+func on_registered():
+	pass
+
+# called when service has been deregistered
+func on_deregistered():
+	pass
